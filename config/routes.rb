8c6dafcab909a0c56a0b865ad150f124ea4624c8
EC2 Default User <ec2-user@ip-172-31-85-166.ec2.internal>
2022-04-24 07:18:28 +0000

    Initial Commit3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   get 'rails/controller'
   get 'rails/books'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get'top'=>'homes#top'
 end
