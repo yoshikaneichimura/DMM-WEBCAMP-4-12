6d1298c49d5047faaaf9bfc8dea146a59328bd4e
EC2 Default User <ec2-user@ip-172-31-85-166.ec2.internal>
2022-05-01 08:54:10 +0000

    Initial Commit9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,12 @@ class BooksController < ApplicationController
   
   def create
      @book = Book.new(book_params)
-     @book.save
+     if@book.save
      redirect_to books_path(@book.id)
+     else
+     render :new
+   end
+  
   end
 
   def index
@@ -20,6 +24,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+    @book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(@book.id)
+  
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
